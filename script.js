8464a84a0c82cda15fdf778a0a505cec40756350
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-04-25 14:56:38 +0300

    feat: add language change, remove default alt

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ async function getData() {
 `;
   body.appendChild(wrapper);
 
-
   const textarea = document.querySelector(".projects__textarea");
+  let textareaValue = [];
 
   const keyboard = document.getElementById("keyboard");
   keyboard.classList.toggle("latin");
@@ -35,9 +35,7 @@ async function getData() {
     keyboard.appendChild(keyRows);
   }
 
-
   function showBtns() {
-
     createRows();
 
     keyboardKeys.map(({ code, en, ru }) => {
@@ -52,8 +50,6 @@ async function getData() {
       } else {
         keyElement.innerHTML = `${ru}`;
       }
-      // console.log(keyElement.innerHTML);
-
       keyRows.appendChild(keyElement);
 
       const insertLineBreak = ["Backspace", "Delete", "Enter", "ShiftRight"].indexOf(code) !== -1;
@@ -63,62 +59,53 @@ async function getData() {
 
       const letGrow = ["Backspace", "CapsLock", "Enter", "ShiftRight", "ShiftLeft", "Space"].indexOf(code) !== -1;
       if (letGrow) { keyElement.classList.add("long"); }
-
     });
     return keyboardKeys
   }
   showBtns();
-  const rows = document.querySelectorAll(".keyboard__row");
+
   const keys = document.querySelectorAll(".keyboard__key");
 
-
-
-
-
- 
-
-
-
-
-
-  const ctrl = document.getElementById("ControlLeft");
-
-  ctrl.addEventListener('keypress', () => {
-    keyboard.classList.toggle("latin");
-    keyboard.classList.toggle("russian");
-
-    keys.forEach((key) => {
-      key.innerHTML='';
-    })
-    if (keyboard.classList.contains("latin")) {
-      for(let i=0; i<keys.length; i++){
-        keys[i].innerHTML=`${keyboardKeys[i].en}`;
-      }
-    } else {
-      for(let i=0; i<keys.length; i++){
-        keys[i].innerHTML=`${keyboardKeys[i].ru}`;
+  document.onkeydown = function (event) {
+    textarea.blur();
+    document.getElementById(event.code).classList.add("active");
+      // alt поведение по умолчание - отключено
+    if (event.altKey) {
+      event.preventDefault();
+    }
+    // CTRL & alt сменя языка
+    if (event.ctrlKey && event.altKey) {
+      keyboard.classList.toggle("latin");
+      keyboard.classList.toggle("russian");
+      console.log('нажат ctrl');
+      if (keyboard.classList.contains("latin")) {
+        for (let i = 0; i < keys.length; i++) {
+          keys[i].innerHTML = `${keyboardKeys[i].en}`;
+        }
+      } else {
+        for (let i = 0; i < keys.length; i++) {
+          keys[i].innerHTML = `${keyboardKeys[i].ru}`;
+        }
       }
     }
-  })
-
-
-  document.onkeydown = function (event) {
-    // console.log(event.code);
-    // console.log(event.key);
-    textarea.focus();
-    keys.forEach(() => {
-      document.getElementById(event.code).classList.add("active");
-      
+// вывод в textarea
+    keys.forEach((item) => {
+      if (event.code == item.id && event.code != 'ControlLeft' && event.code != 'AltLeft') {
+        textareaValue.push(`${item.innerText}`);
+      }
+      else {
+        textareaValue.push();
+      }
+      textarea.innerText = textareaValue.join('');
     });
-    
+    console.log(textareaValue)
+    return textareaValue
   };
+
   document.onkeyup = function (event) {
-    textarea.blur();
     document.getElementById(event.code).classList.remove("active");
   };
 
-
-
-
 }
 getData();
+
