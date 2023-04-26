a3cb3dab852da823638cc3689472b7fd17971080
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-04-26 14:10:48 +0300

    feat: add capslock functionality

--- keyboard.js
+++ keyboard.js
@@ -1,450 +1,579 @@
-[
+export const keyboardKeys = [
   {
     "code": "Backquote",
     "en": "`",
     "ru": "ё",
     "enShift": "~",
-    "ruShift": "Ё"
+    "ruShift": "Ё",
+    "enCaps": "~",
+    "ruCaps": "Ё"
   },
   {
     "code": "Digit1",
     "en": "1",
     "ru": "1",
     "enShift": "!",
-    "ruShift": "!"
+    "ruShift": "!",
+    "enCaps": "1",
+    "ruCaps": "1"
   },
   {
     "code": "Digit2",
     "en": "2",
     "ru": "2",
     "enShift": "@",
-    "ruShift": "\""
+    "ruShift": "\"",
+    "enCaps": "2",
+    "ruCaps": "2"
   },
   {
     "code": "Digit3",
     "en": "3",
     "ru": "3",
     "enShift": "#",
-    "ruShift": "№"
+    "ruShift": "№",
+    "enCaps": "3",
+    "ruCaps": "3"
   },
   {
     "code": "Digit4",
     "en": "4",
     "ru": "4",
     "enShift": "$",
-    "ruShift": ";"
+    "ruShift": ";",
+    "enCaps": "4",
+    "ruCaps": "4"
   },
   {
     "code": "Digit5",
     "en": "5",
     "ru": "5",
     "enShift": "%",
-    "ruShift": "%"
+    "ruShift": "%",
+    "enCaps": "5",
+    "ruCaps": "5"
   },
   {
     "code": "Digit6",
     "en": "6",
     "ru": "6",
     "enShift": "^",
-    "ruShift": ":"
+    "ruShift": ":",
+    "enCaps": "6",
+    "ruCaps": "6"
   },
   {
     "code": "Digit7",
     "en": "7",
     "ru": "7",
     "enShift": "&",
-    "ruShift": "?"
+    "ruShift": "?",
+    "enCaps": "7",
+    "ruCaps": "8"
   },
   {
     "code": "Digit8",
     "en": "8",
     "ru": "8",
     "enShift": "*",
-    "ruShift": "*"
+    "ruShift": "*",
+    "enCaps": "8",
+    "ruCaps": "8"
   },
   {
     "code": "Digit9",
     "en": "9",
     "ru": "9",
     "enShift": "(",
-    "ruShift": "("
+    "ruShift": "(",
+    "enCaps": "9",
+    "ruCaps": "9"
   },
   {
     "code": "Digit0",
     "en": "0",
     "ru": "0",
     "enShift": ")",
-    "ruShift": ")"
+    "ruShift": ")",
+    "enCaps": "0",
+    "ruCaps": "0"
   },
   {
     "code": "Minus",
     "en": "-",
     "ru": "-",
     "enShift": "_",
-    "ruShift": "_"
+    "ruShift": "_",
+    "enCaps": "-",
+    "ruCaps": "-"
   },
   {
     "code": "Equal",
     "en": "=",
     "ru": "=",
     "enShift": "+",
-    "ruShift": "+"
+    "ruShift": "+",
+    "enCaps": "=",
+    "ruCaps": "="
   },
   {
     "code": "Backspace",
     "en": "Backspace",
     "ru": "Backspace",
     "enShift": "Backspace",
-    "ruShift": "Backspace"
+    "ruShift": "Backspace",
+    "enCaps": "Backspace",
+    "ruCaps": "Backspace"
   },
   {
     "code": "Tab",
     "en": "Tab",
     "ru": "Tab",
     "enShift": "Tab",
-    "ruShift": "Tab"
+    "ruShift": "Tab",
+    "enCaps": "Tab",
+    "ruCaps": "Tab"
   },
   {
     "code": "KeyQ",
     "en": "q",
     "ru": "й",
     "enShift": "Q",
-    "ruShift": "Й"
+    "ruShift": "Й",
+    "enCaps": "Q",
+    "ruCaps": "Й"
   },
   {
     "code": "KeyW",
     "en": "w",
     "ru": "ц",
     "enShift": "W",
-    "ruShift": "Ц"
+    "ruShift": "Ц",
+    "enCaps": "W",
+    "ruCaps": "Ц"
   },
   {
     "code": "KeyE",
     "en": "e",
     "ru": "у",
     "enShift": "E",
-    "ruShift": "У"
+    "ruShift": "У",
+    "enCaps": "E",
+    "ruCaps": "У"
   },
   {
     "code": "KeyR",
     "en": "r",
     "ru": "к",
     "enShift": "R",
-    "ruShift": "К"
+    "ruShift": "К",
+    "enCaps": "R",
+    "ruCaps": "К"
   },
   {
     "code": "KeyT",
     "en": "t",
     "ru": "е",
     "enShift": "T",
-    "ruShift": "Е"
+    "ruShift": "Е",
+    "enCaps": "T",
+    "ruCaps": "Е"
   },
   {
     "code": "KeyY",
     "en": "y",
     "ru": "н",
     "enShift": "Y",
-    "ruShift": "Н"
+    "ruShift": "Н",
+    "enCaps": "Y",
+    "ruCaps": "Н"
   },
   {
     "code": "KeyU",
     "en": "u",
     "ru": "г",
     "enShift": "U",
-    "ruShift": "Г"
+    "ruShift": "Г",
+    "enCaps": "U",
+    "ruCaps": "Г"
   },
   {
     "code": "KeyI",
     "en": "i",
     "ru": "ш",
     "enShift": "I",
-    "ruShift": "Ш"
+    "ruShift": "Ш",
+    "enCaps": "I",
+    "ruCaps": "Ш"
   },
   {
     "code": "KeyO",
     "en": "o",
     "ru": "щ",
     "enShift": "O",
-    "ruShift": "Щ"
+    "ruShift": "Щ",
+    "enCaps": "O",
+    "ruCaps": "Щ"
   },
   {
     "code": "KeyP",
     "en": "p",
     "ru": "з",
     "enShift": "P",
-    "ruShift": "З"
+    "ruShift": "З",
+    "enCaps": "P",
+    "ruCaps": "З"
   },
   {
     "code": "BracketLeft",
     "en": "[",
     "ru": "х",
     "enShift": "{",
-    "ruShift": "Х"
+    "ruShift": "Х",
+    "enCaps": "[",
+    "ruCaps": "Х"
   },
   {
     "code": "BracketRight",
     "en": "]",
     "ru": "ъ",
     "enShift": "}",
-    "ruShift": "Ъ"
+    "ruShift": "Ъ",
+    "enCaps": "]",
+    "ruCaps": "Ъ"
   },
   {
     "code": "Backslash",
     "en": "\\",
     "ru": "\\",
     "enShift": "|",
-    "ruShift": "/"
+    "ruShift": "/",
+    "enCaps": "\\",
+    "ruCaps": "/"
   },
   {
     "code": "Delete",
     "en": "Del",
     "ru": "Del",
     "enShift": "Del",
-    "ruShift": "Del"
+    "ruShift": "Del",
+    "enCaps": "Del",
+    "ruCaps": "Del"
   },
   {
     "code": "CapsLock",
     "en": "Caps Lock",
     "ru": "Caps Lock",
     "enShift": "Caps Lock",
-    "ruShift": "Caps Lock"
+    "ruShift": "Caps Lock",
+    "enCaps": "Caps Lock",
+    "ruCaps": "Caps Lock"
   },
   {
     "code": "KeyA",
     "en": "a",
     "ru": "ф",
     "enShift": "A",
-    "ruShift": "Ф"
+    "ruShift": "Ф",
+    "enCaps": "A",
+    "ruCaps": "Ф"
   },
   {
     "code": "KeyS",
     "en": "s",
     "ru": "ы",
     "enShift": "S",
-    "ruShift": "Ы"
+    "ruShift": "Ы",
+    "enCaps": "S",
+    "ruCaps": "Ы"
   },
   {
     "code": "KeyD",
     "en": "d",
     "ru": "в",
     "enShift": "D",
-    "ruShift": "В"
+    "ruShift": "В",
+    "enCaps": "D",
+    "ruCaps": "В"
   },
   {
     "code": "KeyF",
     "en": "f",
     "ru": "а",
     "enShift": "F",
-    "ruShift": "А"
+    "ruShift": "А",
+    "enCaps": "F",
+    "ruCaps": "А"
   },
   {
     "code": "KeyG",
     "en": "g",
     "ru": "п",
     "enShift": "G",
-    "ruShift": "П"
+    "ruShift": "П",
+    "enCaps": "G",
+    "ruCaps": "П"
   },
   {
     "code": "KeyH",
     "en": "h",
     "ru": "р",
     "enShift": "H",
-    "ruShift": "Р"
+    "ruShift": "Р",
+    "enCaps": "H",
+    "ruCaps": "Р"
   },
   {
     "code": "KeyJ",
     "en": "j",
     "ru": "о",
     "enShift": "J",
-    "ruShift": "О"
+    "ruShift": "О",
+    "enCaps": "J",
+    "ruCaps": "О"
   },
   {
     "code": "KeyK",
     "en": "k",
     "ru": "л",
     "enShift": "K",
-    "ruShift": "Л"
+    "ruShift": "Л",
+    "enCaps": "K",
+    "ruCaps": "Л"
   },
   {
     "code": "KeyL",
     "en": "l",
     "ru": "д",
     "enShift": "L",
-    "ruShift": "Д"
+    "ruShift": "Д",
+    "enCaps": "L",
+    "ruCaps": "Д"
   },
   {
     "code": "Semicolon",
     "en": ";",
     "ru": "ж",
     "enShift": ":",
-    "ruShift": "Ж"
+    "ruShift": "Ж",
+    "enCaps": ";",
+    "ruCaps": "Ж"
   },
   {
     "code": "Quote",
     "en": "'",
     "ru": "э",
     "enShift": "\"",
-    "ruShift": "Э"
+    "ruShift": "Э",
+    "enCaps": "'",
+    "ruCaps": "Э"
   },
   {
     "code": "Enter",
     "en": "Enter",
     "ru": "Enter",
     "enShift": "Enter",
-    "ruShift": "Enter"
+    "ruShift": "Enter",
+    "enCaps": "Enter",
+    "ruCaps": "Enter"
   },
   {
     "code": "ShiftLeft",
     "en": "Shift",
     "ru": "Shift",
     "enShift": "Shift",
-    "ruShift": "Shift"
+    "ruShift": "Shift",
+    "enCaps": "Shift",
+    "ruCaps": "Shift"
   },
   {
     "code": "KeyZ",
     "en": "z",
     "ru": "я",
     "enShift": "Z",
-    "ruShift": "Я"
+    "ruShift": "Я",
+    "enCaps": "Z",
+    "ruCaps": "Я"
   },
   {
     "code": "KeyX",
     "en": "x",
     "ru": "ч",
     "enShift": "X",
-    "ruShift": "Ч"
+    "ruShift": "Ч",
+    "enCaps": "X",
+    "ruCaps": "Ч"
   },
   {
     "code": "KeyC",
     "en": "c",
     "ru": "с",
     "enShift": "C",
-    "ruShift": "С"
+    "ruShift": "С",
+    "enCaps": "C",
+    "ruCaps": "С"
   },
   {
     "code": "KeyV",
     "en": "v",
     "ru": "м",
     "enShift": "V",
-    "ruShift": "М"
+    "ruShift": "М",
+    "enCaps": "V",
+    "ruCaps": "М"
   },
   {
     "code": "KeyB",
     "en": "b",
     "ru": "и",
     "enShift": "B",
-    "ruShift": "И"
+    "ruShift": "И",
+    "enCaps": "B",
+    "ruCaps": "И"
   },
   {
     "code": "KeyN",
     "en": "n",
     "ru": "т",
     "enShift": "N",
-    "ruShift": "Т"
+    "ruShift": "Т",
+    "enCaps": "N",
+    "ruCaps": "Т"
   },
   {
     "code": "KeyM",
     "en": "m",
     "ru": "ь",
     "enShift": "M",
-    "ruShift": "Ь"
+    "ruShift": "Ь",
+    "enCaps": "M",
+    "ruCaps": "Ь"
   },
   {
     "code": "Comma",
     "en": ",",
     "ru": "б",
     "enShift": "<",
-    "ruShift": "Б"
+    "ruShift": "Б",
+    "enCaps": ",",
+    "ruCaps": "Б"
   },
   {
     "code": "Period",
     "en": ".",
     "ru": "ю",
     "enShift": ">",
-    "ruShift": "Ю"
+    "ruShift": "Ю",
+    "enCaps": ".",
+    "ruCaps": "Ю"
   },
   {
     "code": "Slash",
     "en": "/",
     "ru": ".",
     "enShift": "?",
-    "ruShift": ","
+    "ruShift": ",",
+    "enCaps": "/",
+    "ruCaps": "."
   },
   {
     "code": "ArrowUp",
     "en": "⯅",
     "ru": "⯅",
     "enShift": "⯅",
-    "ruShift": "⯅"
+    "ruShift": "⯅",
+    "enCaps": "⯅",
+    "ruCaps": "⯅"
   },
   {
     "code": "ShiftRight",
     "en": "Shift",
     "ru": "Shift",
     "enShift": "Shift",
-    "ruShift": "Shift"
+    "ruShift": "Shift",
+    "enCaps": "Shift",
+    "ruCaps": "Shift"
   },
   {
     "code": "ControlLeft",
     "en": "Ctrl",
     "ru": "Ctrl",
     "enShift": "Ctrl",
-    "ruShift": "Ctrl"
+    "ruShift": "Ctrl",
+    "enCaps": "Ctrl",
+    "ruCaps": "Ctrl"
   },
   {
     "code": "MetaLeft",
     "en": "Win",
     "ru": "Win",
     "enShift": "Win",
-    "ruShift": "Win"
+    "ruShift": "Win",
+    "enCaps": "Win",
+    "ruCaps": "Win"
   },
   {
     "code": "AltLeft",
     "en": "Alt",
     "ru": "Alt",
     "enShift": "Alt",
-    "ruShift": "Alt"
+    "ruShift": "Alt",
+    "enCaps": "Alt",
+    "ruCaps": "Alt"
   },
   {
     "code": "Space",
     "en": " ",
     "ru": " ",
     "enShift": " ",
-    "ruShift": " "
+    "ruShift": " ",
+    "enCaps": " ",
+    "ruCaps": " "
   },
   {
     "code": "AltRight",
     "en": "Alt",
     "ru": "Alt",
     "enShift": "Alt",
-    "ruShift": "Alt"
+    "ruShift": "Alt",
+    "enCaps": "Alt",
+    "ruCaps": "Alt"
   },
   {
     "code": "ArrowLeft",
     "en": "⯇",
     "ru": "⯇",
     "enShift": "⯇",
-    "ruShift": "⯇"
+    "ruShift": "⯇",
+    "enCaps": "⯇",
+    "ruCaps": "⯇"
   },
   {
     "code": "ArrowDown",
     "en": "⯆",
     "ru": "⯆",
     "enShift": "⯆",
-    "ruShift": "⯆"
+    "ruShift": "⯆",
+    "enCaps": "⯆",
+    "ruCaps": "⯆"
   },
   {
     "code": "ArrowRight",
     "en": "⯈",
     "ru": "⯈",
     "enShift": "⯈",
-    "ruShift": "⯈"
+    "ruShift": "⯈",
+    "enCaps": "⯈",
+    "ruCaps": "⯈"
   },
   {
     "code": "ControlRight",
     "en": "Ctrl",
     "ru": "Ctrl",
     "enShift": "Ctrl",
-    "ruShift": "Ctrl"
+    "ruShift": "Ctrl",
+    "enCaps": "Ctrl",
+    "ruCaps": "Ctrl"
   }
 ]
+
--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-async function getData() {
-  const data = "./keyboard.json";
-  const res = await fetch(data);
-  const keyboardKeys = await res.json();
+import { keyboardKeys } from './keyboard.js'
 
-  const { body } = document;
+const { body } = document;
 
-  const wrapper = document.createElement("div");
-  wrapper.className = "wrapper";
-  wrapper.innerHTML = `
+const wrapper = document.createElement("div");
+wrapper.className = "wrapper";
+wrapper.innerHTML = `
     <header class="header">
       <p class="header__title">RSS Виртуальная клавиатура</p>
       <p class="header__subtitle">Клавиатура создана в операционной системе Windows</p>
@@ -20,91 +17,149 @@ async function getData() {
       </div>
     </main>
 `;
-  body.appendChild(wrapper);
+body.appendChild(wrapper);
 
-  const textarea = document.querySelector(".projects__textarea");
-  let textareaValue = [];
+const textarea = document.querySelector(".projects__textarea");
+let textareaValue = [];
 
-  const keyboard = document.getElementById("keyboard");
-  keyboard.classList.toggle("latin");
+const keyboard = document.getElementById("keyboard");
+keyboard.classList.toggle("latin");
+keyboard.classList.toggle("regular")
 
-  let keyRows;
-  function createRows() {
-    keyRows = document.createElement("div");
-    keyRows.className = "keyboard__row row";
-    keyboard.appendChild(keyRows);
+
+let keyRows;
+function createRows() {
+  keyRows = document.createElement("div");
+  keyRows.className = "keyboard__row row";
+  keyboard.appendChild(keyRows);
+}
+
+function showBtns() {
+  createRows();
+
+  keyboardKeys.map(({ code, en, ru }) => {
+
+    const keyElement = document.createElement("button");
+    keyElement.setAttribute("type", "button");
+    keyElement.classList.add("keyboard__key");
+    keyElement.id = `${code}`;
+    if (keyboard.classList.contains("latin")) {
+      keyElement.innerHTML = `${en}`;
+    } else {
+      keyElement.innerHTML = `${ru}`;
+    }
+
+    keyRows.appendChild(keyElement);
+
+    const insertLineBreak = ["Backspace", "Delete", "Enter", "ShiftRight"].indexOf(code) !== -1;
+    if (insertLineBreak) {
+      createRows();
+    }
+
+    const letGrow = ["Backspace", "CapsLock", "Enter", "ShiftRight", "ShiftLeft", "Space"].indexOf(code) !== -1;
+    if (letGrow) { keyElement.classList.add("long"); }
+  });
+  return keyboardKeys
+}
+showBtns();
+
+const keys = document.querySelectorAll(".keyboard__key");
+
+document.onkeydown = function (event) {
+
+  textarea.blur();
+  document.getElementById(event.code).classList.add("active");
+
+  // отключение поведения
+  if (event.altKey) {
+    event.preventDefault();
   }
-
-  function showBtns() {
-    createRows();
-
-    keyboardKeys.map(({ code, en, ru }) => {
-
-      const keyElement = document.createElement("button");
-      keyElement.setAttribute("type", "button");
-      keyElement.classList.add("keyboard__key");
-      keyElement.id = `${code}`;
-
-      if (keyboard.classList.contains("latin")) {
-        keyElement.innerHTML = `${en}`;
-      } else {
-        keyElement.innerHTML = `${ru}`;
-      }
-      keyRows.appendChild(keyElement);
-
-      const insertLineBreak = ["Backspace", "Delete", "Enter", "ShiftRight"].indexOf(code) !== -1;
-      if (insertLineBreak) {
-        createRows();
-      }
-
-      const letGrow = ["Backspace", "CapsLock", "Enter", "ShiftRight", "ShiftLeft", "Space"].indexOf(code) !== -1;
-      if (letGrow) { keyElement.classList.add("long"); }
-    });
-    return keyboardKeys
+  if (event.code == "MetaLeft") {
+    event.preventDefault();
   }
-  showBtns();
+  if (event.code == "Tab") {
+    event.preventDefault();
+  }
+  showCapsValues()
 
-  const keys = document.querySelectorAll(".keyboard__key");
 
-  document.onkeydown = function (event) {
+  // CTRL & alt сменя языка
+  changeLanguage()
 
-    textarea.blur();
-    document.getElementById(event.code).classList.add("active");
+  // показать значения с shift
+  showShiftValues()
 
-    // отключение поведения
-    if (event.altKey) {
-      event.preventDefault();
+  // вывод в textarea
+  showText()
+
+
+  console.log(textareaValue)
+  return textareaValue
+
+};
+
+
+
+document.onkeyup = function (event) {
+  document.getElementById(event.code).classList.remove("active");
+  // убрать влияние shift
+
+  if (event.code == 'ShiftLeft' || event.code == 'ShiftRight') {
+    if (keyboard.classList.contains("latin")) {
+      for (let i = 0; i < keys.length; i++) {
+        keys[i].innerHTML = `${keyboardKeys[i].en}`;
+      }
+    } else {
+      for (let i = 0; i < keys.length; i++) {
+        keys[i].innerHTML = `${keyboardKeys[i].ru}`;
+      }
     }
-    if (event.code == "MetaLeft") {
-      event.preventDefault();
-    }
-    if (event.code == "Tab") {
-      event.preventDefault();
-    }
-
-
-    // CTRL & alt сменя языка
-    changeLanguage()
-
-    // показать значения с shift
-    showShiftValues()
-
-    // вывод в textarea
-    showText()
-
-
-    console.log(textareaValue)
-    return textareaValue
-
   };
 
 
 
-  document.onkeyup = function (event) {
-    document.getElementById(event.code).classList.remove("active");
-    // убрать влияние shift
+}
 
-    if (event.code == 'ShiftLeft' || event.code == 'ShiftRight') {
+
+
+
+
+
+
+
+
+
+function changeLanguage() {
+  if (event.ctrlKey && event.altKey) {
+    keyboard.classList.toggle("latin");
+    keyboard.classList.toggle("russian");
+    if (keyboard.classList.contains("latin")) {
+      for (let i = 0; i < keys.length; i++) {
+        keys[i].innerHTML = `${keyboardKeys[i].en}`;
+      }
+    } else {
+      for (let i = 0; i < keys.length; i++) {
+        keys[i].innerHTML = `${keyboardKeys[i].ru}`;
+      }
+    }
+  }
+}
+function showCapsValues() {
+  if (event.code == 'CapsLock') {
+    keyboard.classList.toggle("Caps");
+    keyboard.classList.toggle("regular");
+
+    if (keyboard.classList.contains("Caps")) {
+      if (keyboard.classList.contains("latin")) {
+        for (let i = 0; i < keys.length; i++) {
+          keys[i].innerHTML = `${keyboardKeys[i].enCaps}`;
+        }
+      } else {
+        for (let i = 0; i < keys.length; i++) {
+          keys[i].innerHTML = `${keyboardKeys[i].ruCaps}`;
+        }
+      }
+    } else {
       if (keyboard.classList.contains("latin")) {
         for (let i = 0; i < keys.length; i++) {
           keys[i].innerHTML = `${keyboardKeys[i].en}`;
@@ -114,72 +169,77 @@ async function getData() {
           keys[i].innerHTML = `${keyboardKeys[i].ru}`;
         }
       }
-    };
-  }
 
-
-
-
-
-
-
-
-
-  function changeLanguage() {
-    if (event.ctrlKey && event.altKey) {
-      keyboard.classList.toggle("latin");
-      keyboard.classList.toggle("russian");
-      if (keyboard.classList.contains("latin")) {
-        for (let i = 0; i < keys.length; i++) {
-          keys[i].innerHTML = `${keyboardKeys[i].en}`;
-        }
-      } else {
-        for (let i = 0; i < keys.length; i++) {
-          keys[i].innerHTML = `${keyboardKeys[i].ru}`;
-        }
-      }
     }
-  }
-
-  function showShiftValues() {
-    if (event.shiftKey) {
-      if (keyboard.classList.contains("latin")) {
-        for (let i = 0; i < keys.length; i++) {
-          keys[i].innerHTML = `${keyboardKeys[i].enShift}`;
-        }
-      } else {
-        for (let i = 0; i < keys.length; i++) {
-          keys[i].innerHTML = `${keyboardKeys[i].ruShift}`;
-        }
-      }
-    }
-  }
-
-  function showText() {
-    keys.forEach((item) => {
-      if (event.code == item.id
-        && event.code != 'ControlLeft' && event.code != 'ControlRight'
-        && event.code != 'AltLeft' && event.code != 'AltRight'
-        && event.code != 'MetaLeft'
-        && event.code != 'ShiftLeft' && event.code != 'ShiftRight'
-        && event.code != 'Tab') {
-        textareaValue.push(`${item.innerText}`);
-      }
-      // поведение при пробеле
-      if (event.code == item.id
-        && event.code == "Space") {
-        textareaValue.push(' ');
-      }
-      if (event.code == item.id
-        && event.code == "Tab") {
-        textareaValue.push('    ');
-      }
-      else {
-        textareaValue.push();
-      }
-      textarea.innerText = textareaValue.join('');
-    });
   }
 }
 
-getData();
+function showShiftValues() {
+  if (event.shiftKey) {
+    if (keyboard.classList.contains("latin")) {
+      for (let i = 0; i < keys.length; i++) {
+        keys[i].innerHTML = `${keyboardKeys[i].enShift}`;
+      }
+    } else {
+      for (let i = 0; i < keys.length; i++) {
+        keys[i].innerHTML = `${keyboardKeys[i].ruShift}`;
+      }
+    }
+  }
+}
+
+function showText() {
+  keys.forEach((item) => {
+    if (event.code == item.id
+      && event.code != 'ControlLeft' && event.code != 'ControlRight'
+      && event.code != 'AltLeft' && event.code != 'AltRight'
+      && event.code != 'MetaLeft'
+      && event.code != 'ShiftLeft' && event.code != 'ShiftRight'
+      && event.code != 'Tab'
+      && event.code != 'Backspace'
+      && event.code != 'CapsLock') {
+      textareaValue.push(`${item.innerText}`);
+    }
+    // поведение при пробеле
+    if (event.code == item.id
+      && event.code == "Space") {
+      textareaValue.push(' ');
+    }
+    if (event.code == item.id
+      && event.code == "Tab") {
+      textareaValue.push('    ');
+    }
+    if (event.code == item.id
+      && event.code == "Backspace") {
+      textareaValue.pop();
+    }
+    if (event.code == item.id
+      && event.code == "CapsLock") {
+      textarea.innerText = textareaValue.join('').toUpperCase();
+    }
+    else {
+      textareaValue.push();
+    }
+    textarea.innerText = textareaValue.join('');
+  });
+}
+
+
+
+
+
+// function setLocalStorage() {
+//   localStorage.setItem('class', keyboard.classList);
+// }
+// window.addEventListener('beforeunload', setLocalStorage);
+
+// //востановление данных
+
+// function getLocalStorage() {
+//   if (localStorage.getItem('class')) {
+//     keyboard.classList = localStorage.getItem('class');
+//   }
+//   console.log(keyboard)
+// }
+// window.addEventListener('load', getLocalStorage);
+
